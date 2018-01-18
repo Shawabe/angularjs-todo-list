52ad1402590183d9ee836111aeb795a3615422f2
Shawabe Shafai <shawabe@virginmedia.com>
2018-01-18 10:59:13 +0000

    3rd commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-angular.module("TodoApp",['ngroute', 'RouteControllers']);
+angular.module("TodoApp",['ngRoute', 'RouteControllers', 'UserService', 'angular-storage', 'TodoService']);
 
 angular.module('TodoApp').config(function($locationProvider, $routeProvider){
 	$locationProvider.html5Mode(true);
@@ -6,9 +6,17 @@ angular.module('TodoApp').config(function($locationProvider, $routeProvider){
 	$routeProvider.when('/', {
 		templateUrl: 'templates/home.html',
 		controller: 'HomeController'
-	});
+	})
 	.when('/accounts/register', {
 		templateUrl: 'templates/register.html',
 		controller: 'RegisterController'
+	})
+	.when('/todo', {
+		templateUrl: 'templates/todo.html',
+		controller: 'TodoController'
+	})
+	.when('/todo/edit/:id', {
+		templateUrl: 'templates/edit-todo.html',
+		controller: 'EditTodoController'
 	});
 });
